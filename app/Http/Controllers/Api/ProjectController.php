1868715a3e596c66512d597f3b35f5520a5e5b5e
AndreaAbbonizio <andreaabbonizio01@gmail.com>
2023-05-31 23:06:39 +0200

    bonus done.

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,20 +5,41 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
 
-        $projects = Project::with('type', 'technologies')->paginate(6);
+        $requestData = $request->all();
+
+        $types = Type::all();
+
+        if ($request->has('type_id') && $requestData['type_id']) {
+
+            $projects = Project::where('type_id', $requestData['type_id'])
+                ->with('type', 'technologies')
+                ->get();
 
 
 
+
+            if (count($projects) == 0) {
+                return response()->json([
+                    'success' => false,
+                    'error' => 'Nessun progetto fa parte di questa tipologia',
+                ]);
+            }
+        } else {
+
+            $projects = Project::with('type', 'technologies')->paginate(6);
+        }
         return response()->json([
             'success' => true,
-            'results' => $projects
+            'results' => $projects,
+            'allTypes' => $types
         ]);
     }
 
